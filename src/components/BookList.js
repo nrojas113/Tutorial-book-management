749a454ebc2c467d8662b54c1cf44a26bb00fee5
Natalie Rojas <natalie.rojas113@gmail.com>
2021-04-15 23:53:24 -0400

    component: Book display & Delete button added

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,31 @@
 import React from "react";
+//lodash is a library that provides utility functions
+import _ from "lodash";
+import Book from "./Book";
 
-const BookList = () => {
-  return <h2>List of Books</h2>;
+const BookList = (props) => {
+  const { books, setBooks } = props;
+  const handleRemoveBook = (id) => {
+    //if delete button is clicked (Book Component),
+    //remove the book with the selected book id
+    setBooks(books.filter((book) => book.id !== id));
+  };
+  return (
+    // Fragment let you group a list of children without adding extra nodes to the DOM
+    //shorter syntax for this is just add <> </> in that case can't pass props
+    <React.Fragment>
+      <div className="book-list">
+        {/* if books is NOT empty, map through the bookx array and render Book component */}
+        {!_.isEmpty(books) ? (
+          books.map((book) => (
+            <Book key={book.id} {...book} handleRemoveBook={handleRemoveBook} />
+          ))
+        ) : (
+          <p className="message">No books available. Please add some books</p>
+        )}
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default BookList;
